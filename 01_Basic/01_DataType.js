d2a550d0df3e37a439eb96545510ce2fab56e6af
kumarankit03 <141125557+kumarankit03@users.noreply.github.com>
2024-05-22 08:16:00 +0000

    Data_type

---
+++ 01_Basic/01_DataType.js
@@ -0,0 +1,23 @@
+"use strict"//treat all js code as newer version
+// alert(3+3)//we are using nodejs , not browser
+console.log(5+4)// code redibality should be high 
+
+let name = "Ankit"
+let age = "18"
+let isloggedIn = false // bool data type
+
+
+//number => 2 to power 53 (range)
+//bigint (for large number)
+// string => ""(use both type with or without bracket)
+//boolean  => true/false
+//null(it is a object type) => standalon value , it  is also used to asssign null value as we use temperature then it value is also 0 degree so in that place we use null
+// undefined => when value is not define
+// symbol => for unique
+
+
+
+//Object
+
+
+console.log(typeof age); //show which type of datatype we are using
