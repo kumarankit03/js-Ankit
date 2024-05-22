d451ad3c77d16ee9c3e9074e1b367da45e023685
kumarankit03 <141125557+kumarankit03@users.noreply.github.com>
2024-05-22 08:17:13 +0000

    01_DataType_Conversion

---
+++ 01_Basic/01_DatatypeConversion.js
@@ -0,0 +1,24 @@
+let score = "12"//string value
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber  );
+console.log(valueInNumber );
+
+
+
+/*
+"22"=>22 string converted into number
+"33abc"=>NAN converted into number but its value  is NAN (Not a Number)
+true = 1 ; false =  0
+*/
+
+let isloggedIn = ""
+let booleanisloggedIn = Boolean(isloggedIn)
+console.log(booleanisloggedIn);
+// 1=>true; 0=>false
+//""  => false
+//"Ankit" or"2"=>true
