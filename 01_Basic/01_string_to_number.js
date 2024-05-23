c586f671b6a2d7c54528165cca22d3c9b455fa25
kumarankit03 <141125557+kumarankit03@users.noreply.github.com>
2024-05-23 20:49:13 +0000

    01_stringg_to_number

---
+++ 01_Basic/01_string_to_number.js
@@ -0,0 +1,28 @@
+let number = -33
+let negativevalue = -number
+console.log(negativevalue)
+console.log(2+20)
+console.log(2-20)
+console.log(2*20)
+console.log(20/2)
+console.log(2**20)// if we use ** ddouble astric then it become the power like its 2 raise to power 20
+
+
+
+let str1 = "hello"
+let str2 = " Ankit"
+let str3 = str1 + str2
+console.log(str3)
+
+
+// it follow the rule of acma script
+console.log("1" + 2);
+console.log("1" + 2);
+console.log("1" + "2")
+console.log("1"+2+2)// if string is in first then  all are treated as string 
+console.log(1+2+"2")//if string is in last then the first iteam is added
+
+
+console.log(true)
+console.log(+true)//because true is a boolean so it conversion take place but increament is done that why it give the value
+//console.log(true+)//give unexpected error beacuse there no value exxist
