977c97a43186085e7ae6b473f8a9200ee719cfd4
kumarankit03 <141125557+kumarankit03@users.noreply.github.com>
2024-05-23 20:48:54 +0000

    01_COD

---
+++ 01_Basic/01_COD.js
@@ -0,0 +1,24 @@
+//comparision of data type
+//console.log(2>1)
+//console.log(2==1)
+//console.log(2!=1)
+//console.log(2<1)
+//console.log(2<=1)
+
+
+// always comapir similar datatype
+console.log("2">1);
+console.log("02">1);
+
+//below part give false as a result
+console.log(null>0);
+console.log(null==0);
+//below part give true result
+console.log(null>=0);
+console.log(null!=0);
+console.log(null>=0);
+
+
+// ===
+//we strictly chheck these by ===
+console.log("2"===2);
